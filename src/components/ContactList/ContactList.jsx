c1384702b2e46ac15b274f0173ba6476d94f8cd3
Yurii Onats <yura.onats@gmail.com>
2022-02-12 17:49:05 +0200

    add spiner

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,9 +3,10 @@ import { ListStyled } from './ContactList.styled';
 import {  useDispatch, useSelector } from 'react-redux'
 import {removeContact} from 'redux/contacts/contacts-operation'
 import toast from 'react-hot-toast';
+import Spiner from "components/Spiner/Spiner"
 
 export default function ContactList() {
-  const {contacts, filter} = useSelector(state=>state.contacts)
+  const {contacts, filter, loading} = useSelector(state=>state.contacts)
   const dispatch = useDispatch();
   
   const deleteContact =(id)=>{
@@ -15,8 +16,8 @@ export default function ContactList() {
   return (
     <>
       <section>
-        <ListStyled>
-          {contacts.length>0 && contacts
+        {loading ? <Spiner /> : <ListStyled>
+          {contacts.length > 0 && contacts
             .filter(contact =>
               contact.name.toLowerCase().includes(filter.toLowerCase())
             )
@@ -30,7 +31,7 @@ export default function ContactList() {
                 />
               </li>
             ))}
-        </ListStyled>
+        </ListStyled>}
       </section>
     </>
   );
---
+++ src/components/Spiner/Spiner.jsx
@@ -0,0 +1,11 @@
+import { TailSpin } from 'react-loader-spinner';
+
+function Spiner() {
+  return (
+    <div>
+      <TailSpin color="#00BFFF" margin="0 auto" height={50} width={50} />
+    </div>
+  );
+}
+
+export default Spiner;
